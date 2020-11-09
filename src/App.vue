662e57d1a1b75cbe5e7f99a2e7b3fee1df893fe8
dataolandu <648752498@qqq.com>
2020-11-09 18:21:54 +0800

    storage封装

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,18 @@
   </div>
 </template>
 
+<script>
+import storage from '../src/storage/index'
+export default {
+    mounted () {
+      // var res = storage.getItem('user').username;
+      // storage.setItem('a',1,);
+      storage.clear('a')
+      // console.log(res);
+    }
+}
+</script>
+
 <style>
 
 </style>
--- src/storage/index.js
+++ src/storage/index.js
@@ -1,1 +1,38 @@
+
+
 // 存储工具箱
+const storage = 'mall';
+export default{
+    setItem(key,value,moudule_name){
+        if(moudule_name){
+           let val = this.getItem(moudule_name);
+           val[key] = value;
+           this.setItem(moudule_name,val);
+        }else{
+            let val = this.getStorage();
+            val[key] = value;
+            console.log(val);
+            window.sessionStorage.setItem(storage,JSON.stringify(val));
+        }
+        
+    },
+    getItem(key){
+        // if(moudule_name){
+        //     let val = this.getItem(moudule_name);
+        //     if(val) return val[key];
+        // }
+        return this.getStorage()[key];
+    },
+    getStorage(){
+        return JSON.parse(window.sessionStorage.getItem(storage)|| '{}')
+    },
+    clear(key,moudule_name){
+        let val = this.getStorage();
+        if(moudule_name){
+            delete val[moudule_name][key];
+        }else{
+            delete val[key];
+        }
+        window.sessionStorage.setItem(storage,JSON.stringify(val));
+    }
+}
